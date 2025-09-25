cd603d548d64323f43f6397a795361c4cae4fde4
hridhwesh-itobuz <hridhwesh@itobuz.com>
2025-09-25 18:40:14 +0530

    first commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const port = process.env.PORT;
+
+const app = express();
+
+const dataBase = "../database/notes.json";
+
+
+app.get("/", (req, res) => {
+  res.send(`"message":"Server is up and running"`);
+});
+
+app.listen(port, () => {
+  console.log(`"message": "Server is up and running on ${port}"`);
+});
